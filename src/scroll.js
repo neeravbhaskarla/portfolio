17847a353a175a0573410098d7a90c6d45141615
= <=>
2021-04-17 10:34:42 +0530

    new code

--- src/scroll.js
+++ src/scroll.js
@@ -2,3 +2,7 @@ const locoScroll = new LocomotiveScroll({
     el: document.querySelector(".scroll-container"),
     smooth: true
   });
+// const but = document.querySelector('#work')
+// document.querySelector('a.button').addEventListener('click',function(){
+//   locoScroll.scrollTo(but)
+// })
